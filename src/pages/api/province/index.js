ea893bc7eac25868b031f892a0d3518dfd5f1100
unknown <danilukman2206@gmail.com>
2024-02-28 14:03:36 +0700

    asd

--- src/pages/api/province/index.js
+++ src/pages/api/province/index.js
@@ -2,17 +2,9 @@ import excuteQuery from 'src/configs/db'
 
 export default async (req, res) => {
   try {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    // const storedToken = req.headers.authorization
     // console.log(storedToken)
     // if (storedToken) {
-
+    console.log(res)
     const result = await excuteQuery({
       query: 'select * from propinsi'
     })
